28c70846f635a7d73311a21fce63d6cbea622e6e
Andrew Mead <andrewjosephmead@gmail.com>
2016-03-01 10:01:29 -0500

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,7 +2,10 @@ var React = require('react');
 
 var Examples = (props) => {
   return (
-    <h3>Examples Component!</h3>
+    <div>
+      <h3>Examples Component!</h3>
+      <p>Welcome to examples page!</p>
+    </div>
   )
 };
 
