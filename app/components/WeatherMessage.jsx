9c8ca2ef9d351a41ff18b66acee828bf64ddaf9c
Andrew Mead <andrewjosephmead@gmail.com>
2016-03-03 11:40:09 -0500

    Add styles to weather page

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 var WeatherMessage = ({temp, location}) => {
   return (
-    <h3>It's it {temp} in {location}.</h3>
+    <h3 className="text-center">It's it {temp} in {location}.</h3>
   )
 };
 
