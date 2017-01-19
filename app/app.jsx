094d96e186e9012c3a11854305e345726f07278a
K.C.Ashish Kumar <kcak11@gmail.com>
2017-01-19 20:11:25 +0530

    Updated Router History Configuration
    Updated Router History Configuration to omit the _k parameter from url

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var {Route, Router, IndexRoute, useRouterHistory} = require('react-router');
+var {createHashHistory} = require('history');
 var Main = require('Main');
 var Weather = require('Weather');
 var About = require('About');
@@ -13,8 +14,10 @@ $(document).foundation();
 // App css
 require('style!css!sass!applicationStyles')
 
+var appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
+
 ReactDOM.render(
-  <Router history={hashHistory}>
+  <Router history={appHistory}>
     <Route path="/" component={Main}>
       <Route path="about" component={About}/>
       <Route path="examples" component={Examples}/>
