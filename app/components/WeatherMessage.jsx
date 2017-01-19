68336051329b87e8bdad134e678948f30f2feac3
K.C.Ashish Kumar <kcak11@gmail.com>
2017-01-19 18:47:51 +0530

    Reorganize the Ghub Folder Structure
    Reorganize the Ghub Folder Structure

--- app/components/WeatherMessage.jsx
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-
-var WeatherMessage = ({temp, location}) => {
-  return (
-    <h3 className="text-center">It's it {temp} in {location}.</h3>
-  )
-};
-
-module.exports = WeatherMessage;
