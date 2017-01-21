0055b1a21f4fff091550defd7f37e2102360e3f6
K.C.Ashish Kumar <kcak11@gmail.com>
2017-01-21 20:20:27 +0530

    .
    .

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -18,7 +18,7 @@ var Nav = React.createClass({
       <div className="top-bar">
         <div className="top-bar-left">
           <ul className="menu">
-            <li className="menu-text">React Weather App</li>
+            <li className="menu-text">Weather App</li>
             <li>
               <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
             </li>
