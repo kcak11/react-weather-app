97ff47a4239b4416464a91565336de4c69d2311d
K.C.Ashish Kumar <kcak11@gmail.com>
2018-08-10 10:16:48 +0530

    Update WeatherMessage.jsx

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 var WeatherMessage = ({temp, location}) => {
   return (
-    <h3 className="text-center">It is {temp}&#176; in {location}.</h3>
+    <h3 className="text-center">It is {temp}&#176;F in {location}.</h3>
   )
 };
 
